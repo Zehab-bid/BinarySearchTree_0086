00aafef6dc2952d8ee597b8f8faabdfe659d8eb8
Zehab-bid <zehabidha152gmail.com>
2025-06-26 18:26:47 +0700

    proges 4

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -31,6 +31,20 @@ public:
         int x;
         cout << "Masukkan nilai: ";
         cin >> x;
-        
+
+        // Step 1: Allocate memory for the new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data field of new node
+        newNode->info = x;
+
+        // Step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        // Step 4: Locate the node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
     }
 }
