2b4b14941bd7b611cd8765e190718643ec81327f
Zehab-bid <zehabidha152gmail.com>
2025-06-26 18:40:30 +0700

    proges 7

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -66,5 +66,17 @@ public:
             // 6b: exit 
             return;
         }
+        
+        // Step 7: If the value in the data field of the new node is greater than of the parent
+        else if (x > parent->info)
+        {
+            // 7a: Make the right child of parent point to the new node
+            parent->rightchild = newNode;
+
+            // 7B: Exit
+            return;
+        }
     }
-}
+
+    void search(int element, Node *&parent, Node *&currentNode)
+};
