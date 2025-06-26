68591450a0d8349f512405942d624364674e1e8a
Zehab-bid <zehabidha152gmail.com>
2025-06-26 18:17:52 +0700

    proges 1

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; // initialize left child to null
+        rightchild = nullptr; // initialize right child to null
+    }
+};
+
+class Binarytree
+{
+public:
+    Node *ROOt;
+}
